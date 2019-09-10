84cc064fca6840a23fd01e911f1068245d0f5b7e
Diego Camargo <dicamargov@gmail.com>
2019-09-10 10:08:21 -0500

    Move the logger to its own module

--- main.go
+++ main.go
@@ -2,33 +2,13 @@ package main
 
 import (
     "context"
+    logger "github.com/belfazt/example-app/logger"
     "log"
+
     "net/http"
-    "os"
 
     "go.uber.org/fx"
 )
-
-// NewLogger constructs a logger. It's just a regular Go function, without any
-// special relationship to Fx.
-//
-// Since it returns a *log.Logger, Fx will treat NewLogger as the constructor
-// function for the standard library's logger. (We'll see how to integrate
-// NewLogger into an Fx application in the main function.) Since NewLogger
-// doesn't have any parameters, Fx will infer that loggers don't depend on any
-// other types - we can create them from thin air.
-//
-// Fx calls constructors lazily, so NewLogger will only be called only if some
-// other function needs a logger. Once instantiated, the logger is cached and
-// reused - within the application, it's effectively a singleton.
-//
-// By default, Fx applications only allow one constructor for each type. See
-// the documentation of the In and Out types for ways around this restriction.
-func NewLogger() *log.Logger {
-    logger := log.New(os.Stdout, "" /* prefix */, 0 /* flags */)
-    logger.Print("Executing NewLogger.")
-    return logger
-}
 
 // NewHandler constructs a simple HTTP handler. Since it returns an
 // http.Handler, Fx will treat NewHandler as the constructor for the
@@ -163,7 +143,7 @@ func main() {
         // Remember that constructors are called lazily, so this block doesn't do
         // much on its own.
         fx.Provide(
-            NewLogger,
+            logger.NewLogger,
             NewHandler,
             NewMux,
         ),
